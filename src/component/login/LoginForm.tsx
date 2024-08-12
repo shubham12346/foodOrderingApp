5edef903524ae3b21879a031eae902a6e1a6376e
shubham agrahari <aakashagrahari26@gmail.com>
2024-08-12 14:07:10 +0530

    Corrected Password name

--- src/component/login/LoginForm.tsx
+++ src/component/login/LoginForm.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "./login.css";
-import { AuthContext } from "../../context/AuthContext";
+import { AuthContext } from "../../AuthContext";
 import { useFormik } from "formik";
 
 const LoginForm = () => {
