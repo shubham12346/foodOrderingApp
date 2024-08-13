d88f102b3b87eb75fdfd66c0b703c12f60cc5aeb
shubham agrahari <aakashagrahari26@gmail.com>
2024-08-13 20:12:44 +0530

    changed app url

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/foodOrderingApp/", // This makes sure paths are relative
+  base: "/foodOrderingApp", // This makes sure paths are relative
 });
